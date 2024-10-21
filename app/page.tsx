d459e48b20054bee124c3a8172576de108c1c309
Igor Sotsugov <234929+sotsugov@users.noreply.github.com>
2024-10-21 19:03:24 +0100

    Wrap usage table

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,11 @@
-import { Suspense } from 'react';
-import { getUsageReports } from './api/usage/getUsage';
-import UsageTable, { TableSkeleton } from './components/usage-table';
+import UsageTableWrapper from '@/components/usage-wrapper';
 
-export default async function Home() {
-  const reports = await getUsageReports();
-
+export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center max-w-2xl">
         <div className="text-4xl font-medium uppercase">solaris</div>
-        <div className="container p-4">
-          <h1 className="font-bold mb-4">Usage Data</h1>
-          <Suspense fallback={<TableSkeleton />}>
-            <UsageTable initialReports={reports} />
-          </Suspense>
-        </div>
+        <UsageTableWrapper />
       </main>
     </div>
   );
